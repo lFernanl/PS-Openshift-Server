47fa177ad75b9c323368176dd4c7b4693e14f590
lFernanl <Blessur_Elcrack@hotmail.com>
2016-05-18 02:04:40 -0430

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -15,7 +15,7 @@ exports.proxyip = ['127.0.0.0/8'];
 //   in every Random Battle team.
 exports.potd = '';
 exports.superAdmins = {
-	"0advy": 1
+	"0abill": 1
 };
 
 // crash guard - write errors to log file instead of crashing
